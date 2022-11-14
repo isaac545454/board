312cd0556e3077a8577bee8d5925e35ad14d1f40
isaac545454 <isaac.newton1221@gmail.com>
2022-11-14 18:23:20 -0300

    feat: header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["sujeitoprogramador.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
