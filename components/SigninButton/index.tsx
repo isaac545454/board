7c1ddf7b6c3317891b22354cc58523fffc161012
isaac545454 <isaac.newton1221@gmail.com>
2022-11-19 18:56:32 -0300

    feat: router protect

--- components/SigninButton/index.tsx
+++ components/SigninButton/index.tsx
@@ -2,29 +2,36 @@ import React from "react";
 import { FaGithub } from "react-icons/Fa";
 import { FiX } from "react-icons/fi";
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function SigninButton() {
-  const session = false;
-  return session === false ? (
+  const { data: session } = useSession();
+  // console.log(session);
+  return session ? (
+    <button
+      className="ml-auto flex text-white items-center"
+      onClick={() => signOut()}
+    >
+      <Image
+        src={session.user.image}
+        alt="steve jobs"
+        width="40"
+        height="40"
+        className="rounded-[50%] mr-4"
+      />
+      ola {session.user.name}
+      <FiX color="#737380" className="ml-4" />
+    </button>
+  ) : (
     <button
       type="button"
-      onClick={() => {}}
+      onClick={() => {
+        signIn("github");
+      }}
       className="ml-auto h-10 rounded-[3rem] bg-transparent flex justify-center items-center text-white px-6 font-bold"
     >
       <FaGithub color="#ffb800" size={25} className="mr-4" />
       Entrar com github
-    </button>
-  ) : (
-    <button className="ml-auto flex text-white">
-      <Image
-        src="https://sujeitoprogramador.com/wp-content/uploads/2022/08/home.png"
-        alt="steve jobs"
-        width="30"
-        height="50"
-        className="rounded-[50%] mr-4"
-      />
-      ola isaac
-      <FiX color="#737380" className="ml-4" />
     </button>
   );
 }
--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import { FiPlus, FiCalendar, FiEdit2, FiTrash, FiClock } from "react-icons/fi";
+import { GetServerSideProps } from "next";
 import Btn from "/components/Btn";
+import { getSession } from "next-auth/react";
 
 export default function board() {
   return (
@@ -71,3 +73,21 @@ export default function board() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  console.log(session);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  console.log(session);
+
+  return {
+    props: {},
+  };
+};
